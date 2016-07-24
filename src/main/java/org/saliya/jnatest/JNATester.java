606cca46d3cf22ad6d02b553a5d1a2a0950bced1
Saliya Ekanayake <esaliya@gmail.com>
2016-07-23 21:51:38 -0400

    first commit

--- src/main/java/org/saliya/jnatest/JNATester.java
+++ src/main/java/org/saliya/jnatest/JNATester.java
@@ -1,8 +1,18 @@
 package org.saliya.jnatest;
 
 public class JNATester {
+
+
     public static void main(String[] args) {
         System.out.println("hello");
+
+        try {
+            Class c = Class.forName("com.sun.jna.Platform");
+            System.out.println("is c==null " + (c==null));
+        } catch (ClassNotFoundException ignored) {
+            System.out.println("error");
+            ignored.printStackTrace();
+        }
 
     }
 }
